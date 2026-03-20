ee985eb77adde6330ccf9c4a2bf999fc3f5c7d13
Nigel Battee <Nigelhbattee@Domain.com>
2026-03-19 17:32:16 -0700

    implemented updates to the Todo app

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -71,13 +71,13 @@ class FormValidator {
       evt.preventDefault();
     });
     this._setEventListeners();
-  }
 }
-export default FormValidator;
 
-// resetValidation();
-// {
-//   this._formElement.reset();
-//   this._inputList.forEach((input) => this._hideInputError(input));
-//   this._toggleButtonState();
-// }
+    resetValidation() {
+    this._formElement.reset();
+    this._inputList.forEach((input) => this._hideInputError(input));
+    this._toggleButtonState();
+}
+}
+
+export default FormValidator;
